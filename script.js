3eb7eb5002958dd5d39d8577e25f7eaa825de985
Stanimir <stanimirkosev00@gmail.com>
2022-03-31 15:02:54 +0300

    16x16 Grid done

--- script.js
+++ script.js
@@ -1,67 +1,20 @@
-    /** First Grid 16x16 - 1 Parent , 16 Child's */
+
+const container=document.getElementById('container');
     
-    const container=document.querySelector('.container');
-    
-        const div1_1_16 = document.createElement('div');
-        div1_1_16.classList.add('div1');
-        container.appendChild(div1_1_16);
+function defaultGrid(rows,cols){
+    for (let i = 0 ; i < ( rows * cols); i++){
+        const div = document.createElement('div');
+        container.appendChild(div).className = 'div1';
+                
+    }
+}
+
+defaultGrid(16,16);
         
-        const div1_2_16 = document.createElement('div');
-        div1_2_16.classList.add('div1');
-        container.appendChild(div1_2_16);
 
-        const div1_3_16 = document.createElement('div');
-        div1_3_16.classList.add('div1');
-        container.appendChild(div1_3_16);
 
-        const div1_4_16 = document.createElement('div');
-        div1_4_16.classList.add('div1');
-        container.appendChild(div1_4_16);
+
+
+
+
     
-        const div1_5_16 = document.createElement('div');
-        div1_5_16.classList.add('div1');
-        container.appendChild(div1_5_16);
-
-        const div1_6_16 = document.createElement('div');
-        div1_6_16.classList.add('div1');
-        container.appendChild(div1_6_16);
-
-        const div1_7_16 = document.createElement('div');
-        div1_7_16.classList.add('div1');
-        container.appendChild(div1_7_16);
-
-        const div1_8_16 = document.createElement('div');
-        div1_8_16.classList.add('div1');
-        container.appendChild(div1_8_16);
-    
-        const div1_9_16 = document.createElement('div');
-        div1_9_16.classList.add('div1');
-        container.appendChild(div1_9_16);
-
-        const div1_10_16 = document.createElement('div');
-        div1_10_16.classList.add('div1');
-        container.appendChild(div1_10_16);
-
-        const div1_11_16 = document.createElement('div');
-        div1_11_16.classList.add('div1');
-        container.appendChild(div1_11_16);
-
-        const div1_12_16 = document.createElement('div');
-        div1_12_16.classList.add('div1');
-        container.appendChild(div1_12_16);
-    
-        const div1_13_16 = document.createElement('div');
-        div1_13_16.classList.add('div1');
-        container.appendChild(div1_13_16);
-
-        const div1_14_16 = document.createElement('div');
-        div1_14_16.classList.add('div1');
-        container.appendChild(div1_14_16);
-
-        const div1_15_16 = document.createElement('div');
-        div1_15_16.classList.add('div1');
-        container.appendChild(div1_15_16);
-
-        const div1_16_16 = document.createElement('div');
-        div1_16_16.classList.add('div1');
-        container.appendChild(div1_16_16);
