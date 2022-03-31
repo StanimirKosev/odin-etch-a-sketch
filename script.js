de09c71d35e213326a8ceb5302a78f1c73c827d3
Stanimir <stanimirkosev00@gmail.com>
2022-03-31 15:54:16 +0300

    Event listener done

--- script.js
+++ script.js
@@ -2,16 +2,20 @@
 const container=document.getElementById('container');
     
 function defaultGrid(rows,cols){
+    
     for (let i = 0 ; i < ( rows * cols); i++){
         const div = document.createElement('div');
         container.appendChild(div).className = 'div1';
-                
+       
+        div.addEventListener('mouseover', function (){
+            div.style.backgroundColor = "#808080";
+        });
     }
+   
 }
 
 defaultGrid(16,16);
-        
-
+ 
 
 
 
